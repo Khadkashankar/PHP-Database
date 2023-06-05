cda8a934320840d2c9d0472b82aaa5c1a39d0e1c
Khadkashankar <syamd5421@gmail.com>
2023-06-05 08:14:29 +0545

    retrieve the data from the table

---
+++ Using MySQLi object oriented/retrieve.php
@@ -0,0 +1,72 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname="mydb";
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Database Connection</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <?php
+    $sql = "SELECT * FROM student";
+    $result = $conn->query($sql);
+    if($result->num_rows > 0){
+        echo '<table class ="table">';
+        echo "<thead>";
+        echo "<tr>";
+        echo "<th>ID</th>";
+        echo "<th>Name</th>";
+        echo "<th>Roll No.</th>";
+        echo "<th>Address</th>";
+        echo "</tr>";
+        echo "</thead>";
+        echo "<tbody>";
+        while($row = $result->fetch_assoc()){
+        echo "<tr>";
+        echo "<td>".$row['id']."</td>";
+        echo "<td>".$row['name']."</td>";
+        echo "<td>".$row['roll']."</td>";
+        echo "<td>".$row['address']."</td>";
+        echo "</tr>";
+        }
+        echo "</tbody>";
+        echo "</table>";
+        }
+        else
+        {
+            echo "0 results";
+        }
+    ?>
+        <?php $conn->close(); ?>
+
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"
+        integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
---
+++ Using PDO/retrieve.php
@@ -0,0 +1,74 @@
+<?php
+$dsn = "mysql:host=localhost;dbname=mydb;";
+$dbuser = "root";
+$dbpass = "";
+
+
+try{
+    $conn = new PDO($dsn, $dbuser, $dbpass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+}
+catch(PDOException $e){
+    echo "connection failed".$e->getMessage();
+}
+
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Database Connection</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <?php
+    $sql = "SELECT * FROM student";
+    $result = $conn->query($sql);
+    if($result->rowCount() > 0){
+        echo '<table class ="table">';
+        echo "<thead>";
+        echo "<tr>";
+        echo "<th>ID</th>";
+        echo "<th>Name</th>";
+        echo "<th>Roll No.</th>";
+        echo "<th>Address</th>";
+        echo "</tr>";
+        echo "</thead>";
+        echo "<tbody>";
+        while($row = $result->fetch(PDO::FETCH_ASSOC)){
+        echo "<tr>";
+        echo "<td>".$row['id']."</td>";
+        echo "<td>".$row['name']."</td>";
+        echo "<td>".$row['roll']."</td>";
+        echo "<td>".$row['address']."</td>";
+        echo "</tr>";
+        }
+        echo "</tbody>";
+        echo "</table>";
+        }
+        else
+        {
+            echo "0 results";
+        }
+    ?>
+        <?php $conn = null; ?>
+
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"
+        integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
